2b81683087e33ec9e540c5f9d447c86eab684b07
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-09 15:20:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+
+var name
+
+function destructivelyAppendKitten(name) {
+  name = 'Ralph'
+  kittens.push(name)
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyPrependKitten(name) {
+  name = 'Bob'
+  kittens.unshift(name)
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function appendKitten(name) {
+  
+}
